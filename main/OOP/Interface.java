32042b5b571a4ab0f5eeb625e8520450038bafac
Satwashil Sambhaji Khot <khotsatwashil@gmail.com>
2026-01-14 13:00:03 +0530

    Add files via upload

---
+++ main/OOP/Interface.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main.OOP;
+
+/**
+ *
+ * @author satwashil
+ */
+
+//innterface is mechanism in java to achieve abstraction there can only be anstract methods in java interface not method body
+// it is used to achieve abstraction and multiple inheritance in java 
+
+public class Interface {
+    
+}
